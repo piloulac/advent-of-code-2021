94d122747899287e02f1bdfd0fd09e6f1ab1bccb
Pierre-Louis Lacorte <placorte@apple.com>
2021-12-01 11:29:30 +0100

    solutions day 1

--- day_1/part_1.rb
+++ day_1/part_1.rb
@@ -4,3 +4,15 @@ require 'set'
 
 file = File.join(File.dirname(__FILE__), 'input.txt')
 input = File.open(file).readlines.map(&:chomp).map(&:to_i)
+
+increased = 0
+
+prev = input.shift
+input.each do |val|
+  if val > prev
+    increased+=1
+  end
+  prev = val
+end
+
+puts increased
--- day_1/part_2.rb
+++ day_1/part_2.rb
@@ -2,3 +2,14 @@
 
 file = File.join(File.dirname(__FILE__), 'input.txt')
 input = File.open(file).readlines.map(&:chomp).map(&:to_i)
+
+WINDOW_SIZE = 3
+increased = 0
+(0...input.length-WINDOW_SIZE).each do |index|
+  a = input[index...index+WINDOW_SIZE]
+  b = input[index+1..index+WINDOW_SIZE]
+
+  increased += 1 if(b.sum > a.sum)
+end
+
+puts increased
