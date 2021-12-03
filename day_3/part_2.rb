eff43910e06f94b64e0298c636787858922e2b24
Pierre-Louis Lacorte <placorte@apple.com>
2021-12-03 17:44:40 +0100

    refactor day 3 part 2

--- day_3/part_2.rb
+++ day_3/part_2.rb
@@ -1,46 +1,34 @@
 # frozen_string_literal: true
 
-
 file = File.join(File.dirname(__FILE__), 'input.txt')
 input = File.open(file).readlines.map(&:chomp)
-INPUT_LENGTH = input[0].length
 
-gamma = ''
-epsilon = ''
-
-def search_oxygen(data, index)
+def search(data, index, &block)
   return data.first if data.length == 1
 
   hash = data.map{|row| row[index]}.tally
+  keep = yield(hash)
 
+  subset = data.select{|row| row[index] == keep}
+  return search(subset, index + 1, &block)
+end
+
+oxygen = search(input, 0) do |hash|
   min_val, max_val = hash.values.minmax
-  keep = if min_val == max_val
+  if min_val == max_val
     '1'
   else
     hash.key(max_val)
   end
-
-  subset = data.select{|row| row[index] == keep}
-  return search_oxygen(subset, index + 1)
 end
 
-def search_co2(data, index)
-  return data.first if data.length == 1
-
-  hash = data.map{|row| row[index]}.tally
-
+co2 = search(input, 0) do |hash|
   min_val, max_val = hash.values.minmax
-  keep = if min_val == max_val
+  if min_val == max_val
     '0'
   else
     hash.key(min_val)
   end
-
-  subset = data.select{|row| row[index] == keep}
-  return search_co2(subset, index + 1)
 end
 
-
-oxygen = search_oxygen(input, 0)
-co2 = search_co2(input, 0)
 puts oxygen.to_i(2) * co2.to_i(2)
